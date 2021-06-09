7a2058b316b07a381c02e473fd017035168aed79
Sérgio Luís de Azevedo Cruz Filho <sergioluisfilho@hotmail.com>
2021-06-08 22:14:22 -0300

    Revert "Update db.js"
    This reverts commit eec4069d8da4436cc652ec9387839562cc6074a9.
    
    Conflicts:
    	src/services/db.js

--- src/services/db.js
+++ src/services/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST || 'localhost',
-    user: process.env.DATABASE_USER || 'root',
-    password: process.env.DATABASE_PASSWORD || '',
-    database: process.env.DATABASE || 'winest',
+    host: process.env.DATABASE_HOST || 'us-cdbr-east-04.cleardb.com',
+    user: process.env.DATABASE_USER || 'b4f7c1cd5da301',
+    password: process.env.DATABASE_PASSWORD || 'f9a56cb1',
+    database: process.env.DATABASE || 'heroku_a202333d65f0114',
 });
 
 

03d8ffbf749eb34ce02d12829af56802f2e4be36
Sérgio Luís de Azevedo Cruz Filho <sergioluisfilho@hotmail.com>
2021-06-08 21:11:59 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-    return res.json({message: 'hello, world'})
+    return res.json({message: 'Hello, world'})
 })
 
 module.exports = routes

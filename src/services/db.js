bc0fe3d158a854cbcb38620cebdcd4e8798b53ed
Sérgio Luís de Azevedo Cruz Filho <sergioluisfilho@hotmail.com>
2021-06-08 22:36:27 -0300

    Update index.js and db.js

--- src/services/db.js
+++ src/services/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST || 'us-cdbr-east-04.cleardb.com',
-    user: process.env.DATABASE_USER || 'b4f7c1cd5da301',
-    password: process.env.DATABASE_PASSWORD || 'f9a56cb1',
-    database: process.env.DATABASE || 'heroku_a202333d65f0114',
+
+    host: process.env.DATABASE_HOST || 'localhost',
+    user: process.env.DATABASE_USER || 'root',
+    password: process.env.DATABASE_PASSWORD || '',
+    database: process.env.DATABASE || 'winest'
+
 });
 
 

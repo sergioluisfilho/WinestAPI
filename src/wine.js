e44674a832112729b3fdd9ee264e11a117fc2d0e
sergi <sergi@DESKTOP-CO86JVF.local>
2021-07-04 16:37:55 -0300

    Update wine.js

--- src/wine.js
+++ src/wine.js
@@ -3,7 +3,7 @@ const { uuid } = require('uuidv4');
 
 module.exports = {
     async getWines(req, res, next) {
-        db.query('SELECT w.id, title, points, price, country, province, region1, region2, description, designation, variety, winery, taster_name, taster_twitter FROM wines AS w JOIN wine_Location AS wl ON w.id = wl.id JOIN wine_data AS wd ON w.id = wd.id JOIN wine_taster AS wt ON w.id = wt.id;', [], async(err, results) => {
+        db.query('SELECT w.id, title, points, price, price_descount, country, province, region1, region2, description, designation, variety, winery, taster_name, taster_twitter FROM wines AS w JOIN wine_Location AS wl ON w.id = wl.id JOIN wine_data AS wd ON w.id = wd.id JOIN wine_taster AS wt ON w.id = wt.id;', [], async(err, results) => {
             if(err) return res.status(404).json({message: err})
             return res.json({data: results})  
         })
@@ -48,19 +48,19 @@ module.exports = {
                price, country, province, region1,
                region2, description, designation, variety,
                winery, taster_name, taster_twitter} = req.body;
-        const id = uuid();
         
-        db.query('INSERT INTO wines VALUES(?, ?, ?, ?)', [id, title, points, price,], async(err, results) => {
+        
+        db.query('INSERT INTO wines (title, points, price, price_descount) VALUES( ?, ?, ?, ?)', [title, points, price, null], async(err, results) => {
             if(err) return res.status(404).json({message: err})
             else{
                 
-                db.query('INSERT INTO wine_location VALUES(?, ?, ?, ?, ?);', [id, country, province, region1, region2], async(err, results) =>{
+                db.query('INSERT INTO wine_location (country, province, region1, region2) VALUES( ?, ?, ?, ?);', [country, province, region1, region2], async(err, results) =>{
                     if(err) return res.status(404).json({message: err})
                     else {
-                        db.query('INSERT INTO wine_data VALUES(?, ?, ?, ?, ?);', [id, description, designation, variety, winery],async(err, results) =>{
+                        db.query('INSERT INTO wine_data (description, designation, variety, winery) VALUES( ?, ?, ?, ?);', [description, designation, variety, winery],async(err, results) =>{
                             if(err) return res.status(404).json({message: err})
                             else{
-                                db.query('INSERT INTO wine_taster VALUES(?, ?, ?);', [id, taster_name, taster_twitter],async(err, results) =>{
+                                db.query('INSERT INTO wine_taster (taster_name, taster_twitter) VALUES( ?, ?);', [taster_name, taster_twitter],async(err, results) =>{
                                     if(err) return res.status(404).json({message: err})
                                     else{
                                         return res.json({
@@ -92,7 +92,7 @@ module.exports = {
         // })
 
         
-        await  db.query('UPDATE wines SET title = ?, points = ?, price = ? where id = ?;', [title, points, price, id], async(err, results) => {
+        await  db.query('UPDATE wines SET title = ?, points = ?, price = ?, price_descount = ? where id = ?;', [title, points, price, (price * 0.9), id], async(err, results) => {
             if(err) return res.status(404).json({message: err})
             else{
                 db.query('UPDATE wine_data SET description = ?, designation = ?, variety = ?, winery = ? where id = ?;', [description, designation, variety, winery, id], async(err, results) => {
